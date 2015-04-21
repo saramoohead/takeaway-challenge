0567c5336d3c49fe905d396f1353d13b271402b3
Sara OC <saramoo@hotmail.com>
2015-04-21 16:48:39 +0100

    line item takes quantity

--- spec/line_item_spec.rb
+++ spec/line_item_spec.rb
@@ -3,7 +3,13 @@ require 'lineitem'
 describe LineItem do
 
   let(:lineitem) { described_class.new('Clockwise', 2) }
-  it 'can take a book name' do
+
+  it 'takes a book name when created' do
     expect(lineitem.name).to eq('Clockwise')
   end
+
+  it 'takes a quantity when created' do
+    expect(lineitem.quantity).to eq(2)
+  end
+
 end
