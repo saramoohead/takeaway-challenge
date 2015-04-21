bad5ec7f91b0f1d24dd32249d31246a880ea90c0
Sara OC <saramoo@hotmail.com>
2015-04-21 16:16:35 +0100

    create customer with mobile phone number

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,9 +1,10 @@
 class Customer
 
-  attr_reader :first_name
+  attr_reader :first_name, :mobile_number
 
   def initialize options = {}
     @first_name = options.fetch(:first_name)
+    @mobile_number = options.fetch(:mobile_number)
   end
 
 end
--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -2,9 +2,14 @@ require 'customer'
 
 describe Customer do
 
-  let(:customer) { described_class.new(first_name: "Rocky") }
+  let(:customer) { described_class.new(first_name: "Rocky", mobile_number: "07788508417") }
 
   it 'has a name when created' do
     expect(customer.first_name).to eq("Rocky")
   end
+
+  it 'has a mobile phone number' do
+    expect(customer.mobile_number).to eq("07788508417")
+  end
+
 end
